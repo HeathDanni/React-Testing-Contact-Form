d8ebd736d3d67a6c4f17664c4f6dbe908343d1d9
Heather Beckman <heatherbeckman85@gmail.com>
2020-10-08 11:53:44 -0600

    used testing to find out that first name input was restricted to 3 chars, changed this

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, fireEvent, screen, getByRole, getByLabelText } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
-test("render ContactForm", () => {
+test("can submit ContactForm", () => {
     render(<ContactForm />);
 
     const firstNameInput = screen.getByLabelText("First Name*");
@@ -10,5 +10,19 @@ test("render ContactForm", () => {
     const emailInput = screen.getByLabelText("Email*");
     const messageInput = screen.getByLabelText("Message");
     
+    fireEvent.change(firstNameInput, {target: {value: "heather"}});
+    fireEvent.change(lastNameInput, {target: {value: "beckman"}});
+    fireEvent.change(emailInput, {target: {value: "heather@gmail.com"}});
+    fireEvent.change(messageInput, {target: {value: "this is a message"}});
+
+    expect(firstNameInput).toHaveValue("heather");
+    expect(lastNameInput).toHaveValue("beckman");
+    expect(emailInput).toHaveValue("heather@gmail.com");
+    expect(messageInput).toHaveValue("this is a message");
+
+    const submitButton = screen.getByRole("button", {type: /submit/i});
+    fireEvent.click(submitButton);
+
+    const newUser = screen.findByText("\"heather\"");
 
 });
